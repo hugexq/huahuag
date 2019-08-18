7973772304a18023dca90945c21b09cb91e14de6
hugexq <2440711724@qq.com>
2019-08-18 20:30:25 +0800

    huag

--- dem.js
+++ dem.js
@@ -3,4 +3,8 @@
 // git add . 添加文件
 //git commit -m "mode" 提交至远程仓库
 //git push 上传 
+
+/* git reflog 查看所有版本 ，获取版本号 */
+/* git reset --hard  版本号 可以回退到任意一个版本*/
+/* git reset --hard HEAD^ 一个尖角号回退上一个版本 */
 var arr = [1,2,3,4,5,6,7,8];
